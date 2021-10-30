2303cacd42081e839a62dfa60690026704ced205
Stepan Voroncov <svoron87@gmail.com>
2021-10-30 14:11:47 +0300

    project for some tests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { Route, Switch } from 'react-router-dom';
+
+import { Layout } from './components/layout/layout/Layout';
+
+import { AllMeetups } from './pages/AllMeetups';
+import { Favourites } from './pages/Favourites';
+import { NewMeetup } from "./pages/NewMeetup";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetups />
+        </Route>
+        <Route path="/favourites">
+          <Favourites />
+        </Route>
+        <Route path="/addMeetup">
+          <NewMeetup/>
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
